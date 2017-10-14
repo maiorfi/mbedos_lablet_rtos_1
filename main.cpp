c65a316c964f3a40536ff3826c4a034e4516dbc1
Lorenzo Maiorfi <maiorfi@innovactive.it>
2017-10-14 08:51:08 +0200

    Thread separato

--- main.cpp
+++ main.cpp
@@ -9,6 +9,21 @@ static DigitalOut led2(LED2);
 static DigitalOut led3(LED6);
 static DigitalOut led4(LED5);
 
+static Thread s_thread_blink;
+
+void thread_proc_blink(std::vector<DigitalOut> *pleds)
+{
+    while (true)
+    {
+        for (std::vector<DigitalOut>::iterator it = pleds->begin(); it != pleds->end(); it++)
+        {
+            it->write(!it->read());
+
+            wait_ms(125);
+        }
+    }
+}
+
 int main()
 {
     s_leds.push_back(led1);
@@ -16,13 +31,5 @@ int main()
     s_leds.push_back(led3);
     s_leds.push_back(led4);
 
-    while (true)
-    {
-        for (std::vector<DigitalOut>::iterator it=s_leds.begin();it!=s_leds.end();it++)
-        {
-            it->write(!it->read());
-
-            wait_ms(125);
-        }
-    }
+    s_thread_blink.start(callback(thread_proc_blink, &s_leds));
 }
