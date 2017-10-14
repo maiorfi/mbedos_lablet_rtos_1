fd36ba82e07b687be913c51ec735fa7ce49c5610
Lorenzo Maiorfi <maiorfi@innovactive.it>
2017-10-14 09:26:55 +0200

    Esteso esempio con coda eventi schedulata e gestita da un thread separato. Build task configurato in vscode.

--- main.cpp
+++ main.cpp
@@ -9,7 +9,19 @@ static DigitalOut led2(LED2);
 static DigitalOut led3(LED6);
 static DigitalOut led4(LED5);
 
+static Serial s_debug_serial_port(SERIAL_TX, SERIAL_RX);
+
 static Thread s_thread_blink;
+static Thread s_thread_recurrent_1;
+
+static EventQueue s_eq_recurrent_1;
+
+static Timer s_timer_1;
+
+void event_proc_recurrent_1(char c)
+{
+    printf("[event_proc_recurrent_1] Elapsed %d ms\n",s_timer_1.read_ms());
+}
 
 void thread_proc_blink(std::vector<DigitalOut> *pleds)
 {
@@ -26,10 +38,17 @@ void thread_proc_blink(std::vector<DigitalOut> *pleds)
 
 int main()
 {
+    s_debug_serial_port.baud(115200);
+    
     s_leds.push_back(led1);
     s_leds.push_back(led2);
     s_leds.push_back(led3);
     s_leds.push_back(led4);
 
+    s_eq_recurrent_1.call_every(1000,event_proc_recurrent_1,'.');
+
     s_thread_blink.start(callback(thread_proc_blink, &s_leds));
+
+    s_timer_1.start();
+    s_thread_recurrent_1.start(callback(&s_eq_recurrent_1,&EventQueue::dispatch_forever));
 }
