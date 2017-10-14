4d48e46cc347e045c5e82b85acde6f5d546fd82e
Lorenzo Maiorfi <maiorfi@innovactive.it>
2017-10-14 10:24:22 +0200

    Modifica per compilatore ARM

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 
 #include <vector>
 
-static std::vector<DigitalOut> s_leds;
+static std::vector<DigitalOut*> s_leds;
 
 static DigitalOut led1(LED1);
 static DigitalOut led2(LED2);
@@ -23,13 +23,13 @@ void event_proc_recurrent_1(char c)
     printf("[event_proc_recurrent_1] Elapsed %d ms\n",s_timer_1.read_ms());
 }
 
-void thread_proc_blink(std::vector<DigitalOut> *pleds)
+void thread_proc_blink(std::vector<DigitalOut*> *pleds)
 {
     while (true)
     {
-        for (std::vector<DigitalOut>::iterator it = pleds->begin(); it != pleds->end(); it++)
+        for (std::vector<DigitalOut*>::iterator it = pleds->begin(); it != pleds->end(); it++)
         {
-            it->write(!it->read());
+            (*it)->write(!((*it)->read()));
 
             wait_ms(125);
         }
@@ -40,10 +40,10 @@ int main()
 {
     s_debug_serial_port.baud(115200);
     
-    s_leds.push_back(led1);
-    s_leds.push_back(led2);
-    s_leds.push_back(led3);
-    s_leds.push_back(led4);
+    s_leds.push_back(&led1);
+    s_leds.push_back(&led2);
+    s_leds.push_back(&led3);
+    s_leds.push_back(&led4);
 
     s_eq_recurrent_1.call_every(1000,event_proc_recurrent_1,'.');
 
